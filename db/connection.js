c3b3bcf416ac2ddef3c8fb145a31eaa7f9399746
Nelson Rivera <riveranelson99@gmail.com>
2021-10-27 17:42:18 -0500

    Added necessary files to be worked on and established foundation structure

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    // Your username
+    user: "root",
+    // Your password
+    password: "",
+    database: "employees"
+})
---
+++ index.js
@@ -0,0 +1,55 @@
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+require("console.table");
+
+init();
+
+// Display logo text, load main prompts
+function init() {
+    const logoText = logo({ name: "Employee Manager" }).render();
+
+    console.log(logoText);
+
+    loadMainPrompts();
+}
+
+function loadMainPrompts() {
+    prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "View All Employees By Department",
+                    value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+                },
+                {
+                    name: "View All Employees By Manger",
+                    value: "VIEW_EMPLOYEES_BY_MANGER"
+                },
+                {
+                    name: "Add Employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                {
+                    name: "Remove Employee",
+                    value: "REMOVE_EMPLOYEE"
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "UPDATE_EMPLOYEE_ROLE"
+                },
+                {
+                    name: "Update Employee Manger",
+                    value: "UPDATE_EMPLOYEE_MANAGER"
+                },
+            ]
+        }
+    ])
+}
