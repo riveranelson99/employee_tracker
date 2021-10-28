e1b704df2897789d8e73dd75f96ace96ad619561
Nelson Rivera <riveranelson99@gmail.com>
2021-10-28 00:22:54 -0500

    Added database and table information

--- db/schema.sql
+++ db/schema.sql
@@ -17,10 +17,26 @@
 -- row 7: 7, Sarah, Lourd, Legal Team Lead, Legal, 250000, null
 -- row 8: 8, Tom, Allen, Lawyer, Legal, 190000, Sarah Lourd
 
-DROP DATABASE IF EXISTS ;
-CREATE DATABASE ;
-USE ;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
-CREATE TABLE (
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+);
 
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
 );
